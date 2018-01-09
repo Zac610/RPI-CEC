71e2457e009db9e73f10da9519ec8f78ea285c78
Zac610 <ramas_rtl@hotmail.com>
2018-01-09 23:14:36 +0100

    Added initial files

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+g++ -std=gnu++0x -fPIC -g -Wall -march=armv6 -mfpu=vfp -mfloat-abi=hard -isystem /opt/vc/include/ -isystem /opt/vc/include/interface/vcos/pthreads/ -isystem /opt/vc/include/interface/vmcs_host/linux/ -I/usr/local/include -L /opt/vc/lib -lcec -lbcm_host -ldl rpi-cec.cpp -o rpi-cec
+
---
+++ rpi-cec.cpp
@@ -0,0 +1,208 @@
+#include <libcec/cec.h>
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::string;
+#include <libcec/cecloader.h>
+
+#include "bcm_host.h"
+
+// The main loop will just continue until a ctrl-C is received
+#include <signal.h>
+
+const static string ProgramName = "Raspberry CEC Test";
+
+
+bool exit_now = false;
+void handle_signal(int signal)
+{
+	cout << "Ricevuto segnale #" << signal << endl;
+    exit_now = true;
+}
+
+//CEC::CBCecKeyPressType from cectypes.h
+void on_keypress(void* not_used, const CEC::cec_keypress *msg)
+{
+	std::string key("default");
+	switch( msg->keycode )
+	{
+		case CEC::CEC_USER_CONTROL_CODE_SELECT:{key="SELECT";break;}
+		case CEC::CEC_USER_CONTROL_CODE_UP:{key="UP";break;}
+		case CEC::CEC_USER_CONTROL_CODE_DOWN:{key="DOWN";break;}
+		case CEC::CEC_USER_CONTROL_CODE_LEFT:{key="LEFT";break;}
+		case CEC::CEC_USER_CONTROL_CODE_RIGHT:{key="RIGHT";break;}
+		case CEC::CEC_USER_CONTROL_CODE_RIGHT_UP:{key="RIGHT_UP";break;}
+		case CEC::CEC_USER_CONTROL_CODE_RIGHT_DOWN:{key="RIGHT_DOWN";break;}
+		case CEC::CEC_USER_CONTROL_CODE_LEFT_UP:{key="LEFT_UP";break;}
+		case CEC::CEC_USER_CONTROL_CODE_LEFT_DOWN:{key="LEFT_DOWN";break;}
+		case CEC::CEC_USER_CONTROL_CODE_ROOT_MENU:{key="ROOT_MENU";break;}
+		case CEC::CEC_USER_CONTROL_CODE_SETUP_MENU:{key="SETUP_MENU";break;}
+		case CEC::CEC_USER_CONTROL_CODE_CONTENTS_MENU:{key="CONTENTS_MENU";break;}
+		case CEC::CEC_USER_CONTROL_CODE_FAVORITE_MENU:{key="FAVORITE_MENU";break;}
+		case CEC::CEC_USER_CONTROL_CODE_EXIT:{key="EXIT";break;}
+		case CEC::CEC_USER_CONTROL_CODE_TOP_MENU:{key="TOP_MENU";break;}
+		case CEC::CEC_USER_CONTROL_CODE_DVD_MENU:{key="DVD_MENU";break;}
+		case CEC::CEC_USER_CONTROL_CODE_NUMBER_ENTRY_MODE:{key="NUMBER_ENTRY_MODE";break;}
+		case CEC::CEC_USER_CONTROL_CODE_NUMBER11:{key="NUMBER11";break;}
+		case CEC::CEC_USER_CONTROL_CODE_NUMBER12:{key="NUMBER12";break;}
+		case CEC::CEC_USER_CONTROL_CODE_NUMBER0:{key="NUMBER0";break;}
+		case CEC::CEC_USER_CONTROL_CODE_NUMBER1:{key="NUMBER1";break;}
+		case CEC::CEC_USER_CONTROL_CODE_NUMBER2:{key="NUMBER2";break;}
+		case CEC::CEC_USER_CONTROL_CODE_NUMBER3:{key="NUMBER3";break;}
+		case CEC::CEC_USER_CONTROL_CODE_NUMBER4:{key="NUMBER4";break;}
+		case CEC::CEC_USER_CONTROL_CODE_NUMBER5:{key="NUMBER5";break;}
+		case CEC::CEC_USER_CONTROL_CODE_NUMBER6:{key="NUMBER6";break;}
+		case CEC::CEC_USER_CONTROL_CODE_NUMBER7:{key="NUMBER7";break;}
+		case CEC::CEC_USER_CONTROL_CODE_NUMBER8:{key="NUMBER8";break;}
+		case CEC::CEC_USER_CONTROL_CODE_NUMBER9:{key="NUMBER9";break;}
+		case CEC::CEC_USER_CONTROL_CODE_DOT:{key="DOT";break;}
+		case CEC::CEC_USER_CONTROL_CODE_ENTER:{key="ENTER";break;}
+		case CEC::CEC_USER_CONTROL_CODE_CLEAR:{key="CLEAR";break;}
+		case CEC::CEC_USER_CONTROL_CODE_NEXT_FAVORITE:{key="NEXT_FAVORITE";break;}
+		case CEC::CEC_USER_CONTROL_CODE_CHANNEL_UP:{key="CHANNEL_UP";break;}
+		case CEC::CEC_USER_CONTROL_CODE_CHANNEL_DOWN:{key="CHANNEL_DOWN";break;}
+		case CEC::CEC_USER_CONTROL_CODE_PREVIOUS_CHANNEL:{key="PREVIOUS_CHANNEL";break;}
+		case CEC::CEC_USER_CONTROL_CODE_SOUND_SELECT:{key="SOUND_SELECT";break;}
+		case CEC::CEC_USER_CONTROL_CODE_INPUT_SELECT:{key="INPUT_SELECT";break;}
+		case CEC::CEC_USER_CONTROL_CODE_DISPLAY_INFORMATION:{key="DISPLAY_INFORMATION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_HELP:{key="HELP";break;}
+		case CEC::CEC_USER_CONTROL_CODE_PAGE_UP:{key="PAGE_UP";break;}
+		case CEC::CEC_USER_CONTROL_CODE_PAGE_DOWN:{key="PAGE_DOWN";break;}
+		case CEC::CEC_USER_CONTROL_CODE_POWER:{key="POWER";break;}
+		case CEC::CEC_USER_CONTROL_CODE_VOLUME_UP:{key="VOLUME_UP";break;}
+		case CEC::CEC_USER_CONTROL_CODE_VOLUME_DOWN:{key="VOLUME_DOWN";break;}
+		case CEC::CEC_USER_CONTROL_CODE_MUTE:{key="MUTE";break;}
+		case CEC::CEC_USER_CONTROL_CODE_PLAY:{key="PLAY";break;}
+		case CEC::CEC_USER_CONTROL_CODE_STOP:{key="STOP";break;}
+		case CEC::CEC_USER_CONTROL_CODE_PAUSE:{key="PAUSE";break;}
+		case CEC::CEC_USER_CONTROL_CODE_RECORD:{key="RECORD";break;}
+		case CEC::CEC_USER_CONTROL_CODE_REWIND:{key="REWIND";break;}
+		case CEC::CEC_USER_CONTROL_CODE_FAST_FORWARD:{key="FAST_FORWARD";break;}
+		case CEC::CEC_USER_CONTROL_CODE_EJECT:{key="EJECT";break;}
+		case CEC::CEC_USER_CONTROL_CODE_FORWARD:{key="FORWARD";break;}
+		case CEC::CEC_USER_CONTROL_CODE_BACKWARD:{key="BACKWARD";break;}
+		case CEC::CEC_USER_CONTROL_CODE_STOP_RECORD:{key="STOP_RECORD";break;}
+		case CEC::CEC_USER_CONTROL_CODE_PAUSE_RECORD:{key="PAUSE_RECORD";break;}
+		case CEC::CEC_USER_CONTROL_CODE_ANGLE:{key="ANGLE";break;}
+		case CEC::CEC_USER_CONTROL_CODE_SUB_PICTURE:{key="SUB_PICTURE";break;}
+		case CEC::CEC_USER_CONTROL_CODE_VIDEO_ON_DEMAND:{key="VIDEO_ON_DEMAND";break;}
+		case CEC::CEC_USER_CONTROL_CODE_ELECTRONIC_PROGRAM_GUIDE:{key="ELECTRONIC_PROGRAM_GUIDE";break;}
+		case CEC::CEC_USER_CONTROL_CODE_TIMER_PROGRAMMING:{key="TIMER_PROGRAMMING";break;}
+		case CEC::CEC_USER_CONTROL_CODE_INITIAL_CONFIGURATION:{key="INITIAL_CONFIGURATION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_SELECT_BROADCAST_TYPE:{key="SELECT_BROADCAST_TYPE";break;}
+		case CEC::CEC_USER_CONTROL_CODE_SELECT_SOUND_PRESENTATION:{key="SELECT_SOUND_PRESENTATION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_PLAY_FUNCTION:{key="PLAY_FUNCTION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_PAUSE_PLAY_FUNCTION:{key="PAUSE_PLAY_FUNCTION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_RECORD_FUNCTION:{key="RECORD_FUNCTION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_PAUSE_RECORD_FUNCTION:{key="PAUSE_RECORD_FUNCTION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_STOP_FUNCTION:{key="STOP_FUNCTION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_MUTE_FUNCTION:{key="MUTE_FUNCTION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_RESTORE_VOLUME_FUNCTION:{key="RESTORE_VOLUME_FUNCTION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_TUNE_FUNCTION:{key="TUNE_FUNCTION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_SELECT_MEDIA_FUNCTION:{key="SELECT_MEDIA_FUNCTION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_SELECT_AV_INPUT_FUNCTION:{key="SELECT_AV_INPUT_FUNCTION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_SELECT_AUDIO_INPUT_FUNCTION:{key="SELECT_AUDIO_INPUT_FUNCTION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_POWER_TOGGLE_FUNCTION:{key="POWER_TOGGLE_FUNCTION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_POWER_OFF_FUNCTION:{key="POWER_OFF_FUNCTION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_POWER_ON_FUNCTION:{key="POWER_ON_FUNCTION";break;}
+		case CEC::CEC_USER_CONTROL_CODE_F1_BLUE:{key="F1_BLUE";break;}
+		case CEC::CEC_USER_CONTROL_CODE_F2_RED:{key="F2_RED";break;}
+		case CEC::CEC_USER_CONTROL_CODE_F3_GREEN:{key="F3_GREEN";break;}
+		case CEC::CEC_USER_CONTROL_CODE_F4_YELLOW:{key="F4_YELLOW";break;}
+		case CEC::CEC_USER_CONTROL_CODE_F5:{key="F5";break;}
+		case CEC::CEC_USER_CONTROL_CODE_DATA:{key="DATA";break;}
+		case CEC::CEC_USER_CONTROL_CODE_AN_RETURN:{key="AN_RETURN";break;}
+		case CEC::CEC_USER_CONTROL_CODE_AN_CHANNELS_LIST:{key="AN_CHANNELS_LIST";break;}
+		default: break;
+	};
+
+	std::cout << "on_keypress [" << msg->duration << "]: " << static_cast<int>(msg->keycode) << " " << key << std::endl;
+	return;
+}
+
+
+int main(int argc, char* argv[])
+{
+	cout << ProgramName << " starting..." << endl;
+	
+	// Install the ctrl-C signal handler
+	if( SIG_ERR == signal(SIGINT, handle_signal) )
+	{
+		std::cerr << "Failed to install the SIGINT signal handler\n";
+		return 1;
+	}
+
+	// Initialise the graphics pipeline for the raspberry pi. Yes, this is necessary.
+	bcm_host_init();
+
+	// Set up the CEC config and specify the keypress callback function
+	CEC::ICECCallbacks cec_callbacks;
+	CEC::libcec_configuration cec_config;
+	cec_config.Clear();
+	cec_callbacks.Clear();
+	cec_callbacks.keyPress	= &on_keypress;
+//cec_callbacks.CBCecKeyPress	= &on_keypress;
+
+	const std::string devicename("CECExample");
+	devicename.copy(cec_config.strDeviceName, std::min(devicename.size(),13u) );
+
+	cec_config.clientVersion	 = CEC::LIBCEC_VERSION_CURRENT;
+	cec_config.bActivateSource = 0;
+	cec_config.callbacks		   = &cec_callbacks;
+	cec_config.deviceTypes.Add(CEC::CEC_DEVICE_TYPE_RECORDING_DEVICE);
+	//cec_config.deviceTypes.Add(CEC::CEC_DEVICE_TYPE_PLAYBACK_DEVICE);
+
+	// Get a cec adapter by initialising the cec library
+	CEC::ICECAdapter* cec_adapter = LibCecInitialise(&cec_config);
+	if( !cec_adapter )
+	{ 
+		std::cerr << "Failed loading libcec.so\n"; 
+		return 1; 
+	}
+
+	// Try to automatically determine the CEC devices 
+	//CEC::cec_adapter devices[10];
+CEC::cec_adapter_descriptor devices[10];
+	int8_t devices_found = cec_adapter->DetectAdapters(devices, 10, NULL); // TODO: deprecated, use DetectAdapters instead
+//	int8_t devices_found = cec_adapter->FindAdapters(devices, 10, NULL); // TODO: deprecated, use DetectAdapters instead
+	if( devices_found <= 0)
+	{
+		std::cerr << "Could not automatically determine the cec adapter devices\n";
+		UnloadLibCec(cec_adapter);
+		return 1;
+	}
+	else
+	{
+		cout << "trovati " << (int)devices_found << " dispositivi" << endl;
+		cout << " comm: " << devices[0].strComName << endl;
+		cout << " path: " << devices[0].strComPath << endl;
+	}
+
+	// Open a connection to the zeroth CEC device
+	if( !cec_adapter->Open(devices[0].strComName) )
+	{		
+		std::cerr << "Failed to open the CEC device on port " << devices[0].strComName << std::endl;
+		UnloadLibCec(cec_adapter);
+		return 1;
+	}
+# if 1
+	// Loop until ctrl-C occurs
+	cout << "Ready." << endl;
+	int cicli = 0;
+	//while( !exit_now ) // without while -> segmentation fault
+	while (cicli++ < 30)
+	{
+	 // nothing to do.  All happens in the CEC callback on another thread
+		sleep(1);
+	}
+#endif
+	// Close down and cleanup
+	cec_adapter->Close();
+	UnloadLibCec(cec_adapter);
+
+	cout << ProgramName << " ended." << endl;
+
+	return 0;
+}
+
